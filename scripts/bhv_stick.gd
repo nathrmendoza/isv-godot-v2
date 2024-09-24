6741d6fd978d658b1818fb531f6de7cb9087489c
Nath Mendoza <mendoza.nathaniel328@gmail.com>
2024-09-24 22:32:58 +1200

    updates 09/24/2024: trash can, update to stick drag

--- scripts/bhv_stick.gd
+++ scripts/bhv_stick.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var colliding_body_ref = null
 var ingredient_slots: Array = [null, null, null]
+
 #set to false if stick leaves prep area (avoids adding ingredient outside)
 var prep_state = true
 var mouse_inside = false
@@ -13,7 +14,13 @@ var mouse_inside = false
 ]
 
 @onready var collision_body: CollisionShape2D = $Area2D/CollisionShape2D
+
+const TWEEN_SPEED = 0.45
 var last_position: Vector2 = Vector2.ZERO
+
+var drag_start: Vector2
+var is_dragging: bool = false
+var is_inside_droppable: bool = false
 
 func _ready() -> void:
 	last_position = global_position
@@ -22,14 +29,56 @@ func _ready() -> void:
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			#dragging logic
-			if mouse_inside and not prep_state:
-				print_debug('grab')
+			#dragging
+			if not prep_state:
+				if check_pointer_within_shape(event.global_position):
+					is_dragging = true
+					drag_start = event.global_position - global_position
+					get_tree().call_group('trash', 'tween_show')
 		else:
-			#release ingredient
+			#ingredient drop
 			if prep_state and mouse_inside and colliding_body_ref:
 				add_ingredient(colliding_body_ref)
+			
 
+func _physics_process(delta: float) -> void:
+	if is_dragging:
+		var current_pointer_position = get_global_mouse_position()
+		if Input.is_action_pressed('click'):
+			if not prep_state:
+				global_position = current_pointer_position
+		elif Input.is_action_just_released('click'):
+			if not prep_state:
+				is_dragging = false
+				tween_go_to_position(last_position)
+				get_tree().call_group('trash', 'tween_hide')
+
+#tweens
+func tween_go_to_position(mv_position: Vector2) -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, 'global_position', mv_position, TWEEN_SPEED)
+
+#check if mouse/touch is within collision_body shape
+func check_pointer_within_shape(point: Vector2) -> bool:
+	if collision_body.shape:
+		var local_point = to_local(point)
+		if collision_body.shape is RectangleShape2D:
+			var half_extents = collision_body.shape.size / 2
+			return abs(local_point.x) <= half_extents.x and abs(local_point.y) <= half_extents.y
+		elif collision_body.shape is CircleShape2D:
+			return local_point.length() <= collision_body.shape.radius
+		elif collision_body.shape is CapsuleShape2D:
+			var half_height = collision_body.shape.height / 2 - collision_body.shape.radius
+			#horizontal
+			if abs(local_point.y) <= half_height:
+				return abs(local_point.y) <= collision_body.shape.radius
+			else:
+				#vertical
+				var circle_center = Vector2(0, sign(local_point.y) * half_height)
+				return abs(local_point - circle_center).length() <= collision_body.shape.radius
+	#fallback
+	return false
 
 func add_ingredient(ingredient) -> void:
 	if ingredient_slots.has(null):
@@ -66,14 +115,11 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('ingredients') or body.is_in_group('prep_area') or body.is_in_group('serve_area') or body.is_in_group('trash_area'):
 		colliding_body_ref = body
 
-
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	colliding_body_ref = null
 
-
 func _on_area_2d_mouse_entered() -> void:
 	mouse_inside = true
-
 
 func _on_area_2d_mouse_exited() -> void:
 	mouse_inside = false
--- scripts/bhv_trash.gd
+++ scripts/bhv_trash.gd
@@ -13,22 +13,23 @@ func _ready() -> void:
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed == false:
+		if event.pressed == false and colliding_body_ref:
 			colliding_body_ref.queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	print_debug(body)
 	if body.is_in_group('servable'):
 		colliding_body_ref = body
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	colliding_body_ref = null
 
-func toggle_tween(variation: String) -> void:
+func tween_show() -> void:
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
-	
-	if variation == 'show':
-		tween.tween_property(sprite, 'position:y', 0, tween_duration)
-	elif variation == 'hide':
-		tween.tween_property(sprite, 'position:y', size_y, tween_duration)
-		
+	tween.tween_property(sprite, 'position:y', 0, tween_duration)
+
+func tween_hide() -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(sprite, 'position:y', size_y, tween_duration)
